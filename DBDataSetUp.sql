d0ed94aec7fbe0908c022dad9f0484d06470e55c
Charles Humphrey <charles.humphrey35@yahoo.co.uk>
2018-02-15 23:42:30 +0000

    Add project files.

---
+++ DBDataSetUp.sql
@@ -0,0 +1,23 @@
+﻿
+--insert into meetups values (0,'Birmingham','https://www.meetup.com/brumindies/','https://api.meetup.com/brumindies/events?&sign=true');
+--insert into meetups values (1,'Leicester','https://www.facebook.com/groups/LeicesterGamesNetwork/','');
+--insert into meetups values (2,'Leamington','https://www.meetup.com/Leamington-Unity-Developers/','https://api.meetup.com/Leamington-Unity-Developers/events?&sign=true');
+
+--select * from MeetUps
+
+--insert into MeetUpAliases values(0,'bi');
+--insert into MeetUpAliases values(0,'bru');
+--insert into MeetUpAliases values(0,'brum');
+--insert into MeetUpAliases values(0,'birmingham');
+--insert into MeetUpAliases values(0,'brumindies');
+
+--insert into MeetUpAliases values(1,'lei');
+--insert into MeetUpAliases values(1,'lgn');
+--insert into MeetUpAliases values(1,'leic');
+--insert into MeetUpAliases values(1,'leicester');
+
+--insert into MeetUpAliases values(2,'lem');
+--insert into MeetUpAliases values(2,'lud');
+--insert into MeetUpAliases values(2,'leamington');
+
+--select * from MeetUpAliases
