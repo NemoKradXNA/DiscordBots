d7e7bc9296e8584db31960e1ba8e9a6dd3af4da5
Charles Humphrey <charles.humphrey35@yahoo.co.uk>
2018-02-15 23:49:04 +0000

    test

--- MGDBot/Enums/MeetUpBroadCastTypeEnum.cs
+++ MGDBot/Enums/MeetUpBroadCastTypeEnum.cs
@@ -4,11 +4,14 @@ using System.Text;
 
 namespace MGDBot.Enums
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public enum MeetUpBroadCastTypeEnum : int
     {
         Reoccuring,
         TimeSpanBefore,
         Weekly,
-        Monthly,
+        Monthly, 
     }
 }
