4ed4a5ae52ac35a267b44756b15b40b09ade199a
Charles Humphrey <charles.humphrey35@yahoo.co.uk>
2020-04-01 13:03:12 +0100

    Started work on a traveller BOT for Discord :)

---
+++ SOLBot/SOLBot/CommandModules/TravellerMapAPICommands.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+using SOLBot.BaseClasses;
+
+namespace SOLBot.CommandModules
+{
+    public class TravellerMapAPICommands : CommandModuleBase
+    {
+        public TravellerMapAPICommands() : base() { defaultColor = new Color(0, 71, 171); }
+
+        [Command("travellermap")]
+        [Alias(new string[] { "tm", "map" })]
+        [Summary("This command lists ALL Traveller Map API commands")]
+        public async Task TravellerMap()
+        {
+            string hlp = "Here is the definitive list of Traveller Map Command available to you:-";
+
+            EmbedBuilder Msg = GetMsg(defaultColor, $"Traveller Map API Commands List", hlp);
+
+            Msg.AddField("Universe", "Get a lsit of ALL sectors by using\n~universe", true);
+
+            await Context.Channel.SendMessageAsync("", false, Msg.Build());
+        }
+    }
+}
---
+++ SOLBot/SOLBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using SOLBot.Configuration;
+using SOLBot.Bot;
+using SOLBot.Services;
+
+namespace SOLBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BotConfiguration confiuration = new BotConfiguration();
+
+            TravellerBot bot = new TravellerBot();
+            bot.StartServer(new TravellerService()).GetAwaiter().GetResult();
+
+            Console.ReadKey();
+        }
+    }
+}
